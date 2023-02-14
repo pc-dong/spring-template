3027f36463c54c7f21ede6f639f2d56d539c266c
peichao.dong <peichao.dong@thoughtworks.com>
2023-02-14 16:06:39 +0800

    add jacoco config

---
+++ main/src/main/java/cn/dpc/main/Account.java
@@ -0,0 +1,36 @@
+package cn.dpc.main;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class Account {
+    private AccountId id;
+
+    private AccountDescription description;
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    public static class AccountId {
+        private String id;
+    }
+
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class AccountDescription {
+        private String name;
+
+        private LocalDateTime createdAt;
+
+        private LocalDateTime lastUpdatedAt;
+    }
+}
