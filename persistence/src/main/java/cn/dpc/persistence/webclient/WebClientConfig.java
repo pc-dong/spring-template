382b1686c77438ec14a9f2796a85614d62967067
peichao.dong <peichao.dong@thoughtworks.com>
2023-02-17 18:34:01 +0800

    chore: update config for api

--- persistence/src/main/java/cn/dpc/persistence/webclient/WebClientConfig.java
+++ persistence/src/main/java/cn/dpc/persistence/webclient/WebClientConfig.java
@@ -9,19 +9,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 @RequiredArgsConstructor
 public class WebClientConfig {
-
-    private final WebClients webClients;
-
     @Bean
     WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("https://localhost:7000")
                 .build();
-    }
-
-    @SneakyThrows
-    @Bean
-    CustomerClient postClient() {
-       return webClients.getCustomerClient();
     }
 }
